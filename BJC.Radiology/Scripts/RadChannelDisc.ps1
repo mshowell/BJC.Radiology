29af2b38dbb25caf6586870cfaa94c529bb3b2a2
mhowell6 <mhowell6@csc.com>
2017-01-04 11:29:01 -0500

    Latest Updates

--- BJC.Radiology/Scripts/RadChannelDisc.ps1
+++ BJC.Radiology/Scripts/RadChannelDisc.ps1
@@ -1,21 +1,30 @@
-﻿param($sourceId, $managedEntityId, $computerName, $Debug="true") 
-
+﻿param($sourceId, $managedEntityId, $computerName) 
+$logfile = "c:\windows\temp\mplogfile.log"
+ac $logfile "Begin Radiology Channel Discover"
 $api = new-object -comObject 'MOM.ScriptAPI' 
 $discoveryData = $api.CreateDiscoveryData(0, $SourceId, $ManagedEntityId) 
+
+ac $logfile "Created discovery objects"
 		
-Import-Module “sqlps” -DisableNameChecking
+#Import-Module “sqlps” -DisableNameChecking
+
+#ac $logfile "Import module passed"
 
 #Create a database connection
 $user = "Radiology"
 $pwd = "Radiology"
-$connectionString = "Server=cscpessqldw01;uid=$user; pwd=$pwd;Database=Radiology;Integrated Security=False;"
+$connectionString = "Server=$computerName;uid=$user; pwd=$pwd;Database=Radiology;Integrated Security=False;"
+ac $logfile $connectionString
 
 $connection = New-Object System.Data.SqlClient.SqlConnection
 $connection.ConnectionString = $connectionString
 
+ac $logfile "ready to attempt database connection"
 try
     {
     $connection.Open()
+	ac $logfile "open worked"
+
 	#if successful, look for availability of Radiology database
     try #to open our intermediate processing table and look for unique channels
         {
@@ -24,25 +33,23 @@ try
         $query = "SELECT distinct facility, procedurecode, priority FROM orders2 order by facility, procedurecode, priority"
         $command.CommandText = $query
 
+		ac $logfile "ready to execute discovery query"
         $result = $command.ExecuteReader()
-
+		ac $logfile "query worked"
 
         $table = new-object “System.Data.DataTable”
         $table.Load($result)
-
+		ac $logfile "data loaded"
 		
-        $instance = $discoveryData.CreateClassInstance("$MPElement[Name='BJC.Radiology.Radiology']$") 
-		$instance.AddProperty("$MPElement[Name='Windows!Microsoft.Windows.Computer']/PrincipalName$", $computerName) 
-		$instance.AddProperty("$MPElement[Name='BJC.Radiology.Radiology']/HostServer$", $computerName) 
-		$instance.AddProperty("$MPElement[Name='System!System.Entity']/DisplayName$", 'Radiology - ' + $DisplayName) 
-		$discoveryData.AddInstance($instance) 
-           
+		
        	#if Radiology database found, create the properties for the BJC.Radiology.Radiology instance
         #for each unique channel, create an object to pass back to SCOM
+		ac $logfile "Now start loop through values"
         $table.DefaultView | ForEach-Object -Process   {
 
             $DisplayName = [string]$_.facility + "-" + $_.procedurecode + ": " + $_.priority
-        
+			$message = "Creating new instance for " + $DisplayName
+			#ac $logfile $message
             $instance = $discoveryData.CreateClassInstance("$MPElement[Name='BJC.Radiology.RadiologyChannel']$") 
 			$instance.AddProperty("$MPElement[Name='Windows!Microsoft.Windows.Computer']/PrincipalName$", $computerName) 
 			$instance.AddProperty("$MPElement[Name='BJC.Radiology.RadiologyChannel']/Facility$", $_.facility) 
@@ -52,14 +59,18 @@ try
 			$discoveryData.AddInstance($instance) 
             }
 		
+			ac $logfile "Returning discovery data"
 		$discoveryData
         }
     catch
         {
+		ac $logfile "Something about query failed"
 		$connection.Close()
 		$discoveryData
 		}
+	}
 catch
 	{
+	ac $logfile "Something about connection failed"
 	$discoveryData
 	}
---
+++ BJC.Radiology/Scripts/RadiologyDisc.ps1
@@ -0,0 +1,68 @@
+﻿param($sourceId, $managedEntityId, $computerName) 
+$logfile = "c:\windows\temp\mplogfile.log"
+ac $logfile "Begin Radiology Discover"
+$api = new-object -comObject 'MOM.ScriptAPI' 
+$discoveryData = $api.CreateDiscoveryData(0, $SourceId, $ManagedEntityId) 
+
+#ac $logfile "Created discovery objects"
+		
+#Import-Module “sqlps” -DisableNameChecking
+
+#ac $logfile "Import module passed"
+
+#Create a database connection
+#$user = "Radiology"
+#$pwd = "Radiology"
+#$connectionString = "Server=$computerName;uid=$user; pwd=$pwd;Database=Radiology;Integrated Security=False;"
+#ac $logfile $connectionString
+
+#$connection = New-Object System.Data.SqlClient.SqlConnection
+#$connection.ConnectionString = $connectionString
+
+#ac $logfile "ready to attempt database connection"
+try
+    {
+  #  $connection.Open()
+	if ($computerName = "CSCPESSQLDW01.CSCPESERVICE.COM") {
+		$message = "open worked for $computerName"
+		ac $logfile $message
+
+
+	#if successful, look for availability of Radiology database
+    try #to open our intermediate processing table and look for unique channels
+        {
+		$message = "Creating new instance for " + $computerName
+
+        ac $logfile $message
+		#ac $logfile "create class"
+            $instance = $discoveryData.CreateClassInstance("$MPElement[Name='BJC.Radiology.Radiology']$") 
+		#ac $logfile "Property 1"
+			$instance.AddProperty("$MPElement[Name='Windows!Microsoft.Windows.Computer']/PrincipalName$", $computerName) 
+		#ac $logfile "Property 2"
+			$instance.AddProperty("$MPElement[Name='BJC.Radiology.Radiology']/HostServer$", $computerName) 
+		#ac $logfile "Property 3"
+			$displayname = "Radiology - " + $computerName
+			$instance.AddProperty("$MPElement[Name='System!System.Entity']/DisplayName$", $displayname) 
+		#ac $logfile "Add Instance"
+			$discoveryData.AddInstance($instance) 
+       
+		
+		ac $logfile "Returning discovery data"
+		$discoveryData
+        }
+    catch
+        {
+		#ac $logfile "Something about object creation failed"
+		#$connection.Close()
+		ac $logfile "Returning empty discovery A"
+		$discoveryData
+		}
+	}
+	else {ac $logfile "Not Radiology server"}
+	}
+catch
+	{
+	#ac $logfile "Something about connection failed"
+	ac $logfile "Returning empty discovery B"
+	$discoveryData
+	}
